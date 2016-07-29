5d912b36b33cc3b144af5e7746d72c7b3832a536
sannoso <sanderruben@live.nl>
2016-07-30 00:41:22 +1000

    quick framework

--- Sudokusolver.py
+++ Sudokusolver.py
@@ -2,7 +2,11 @@
 
 
 def main():
-	print('Hello World!')
+	print('This is Sanders Sudokusolver')
+	#input function
+	#calculation function
+	#return of results function
+	#end text
 
 if __name__ == '__main__':
 	main()
